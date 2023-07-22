747524c4afe04543d6da04f447f37882cfa5a61b
mrWojackETH <xabierblockchain@gmail.com>
2023-07-22 13:12:38 +0200

    📰 documentation

--- src/main.rs
+++ src/main.rs
@@ -2,43 +2,44 @@ mod threadpool;
 use threadpool::ThreadPool;
 
 use std::{
+    thread,
     fs,
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
+    net::{TcpListener, TcpStream}, time::Duration,
 };
 
 const LOCALHOST_ADDRESS: &str = "127.0.0.1:7878";
 fn main() {
-    let stack_size_bytse = 4096 * 10; // 10 KB
-    let thread_pool = ThreadPool::build(1000, stack_size_bytse).unwrap();
+    let stack_size_bytes = 4096 * 10;
+    let thread_pool = ThreadPool::build(4, stack_size_bytes).unwrap();
     let server = TcpListener::bind(LOCALHOST_ADDRESS).unwrap();
+    println!("Listening on {LOCALHOST_ADDRESS}...");
 
-    // el metodo incoming nos devuelve un iterator de intentos de conexion
-    // si nos conectamos desde el navegador fallara por que el server no da respuesta
-    // pero detectaremos una conexion
-    // es posible que nos detecte varios intentos de conexion desde un mismo browser, eso es
-    // por que puede que el browser haga mas requests para el favicon o que reintente despues de fallar
     for stream in server.incoming() {
         let stream = stream.unwrap();
-        // para poder manejar las requests en un buen tiempode respuesta, usaremos muchos threads
-        // pero tiene que haber un limite, no podemos permitir que haya threads ilimitados
-        // ya que podria crashear
-        // es por eso que crearemos un thread pool, que son una serie de threads listos para ser
-        // utilizados
-        thread_pool.execute(|| {
-            handle_connection(stream);
-        }).unwrap();
+        thread_pool
+            .execute(|| {
+                handle_connection(stream);
+            })
+            .unwrap();
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream); // con esto parseamos el bufer a texto
-    let request_line = buf_reader.lines().next().unwrap().unwrap(); // le hacemos 2 unwrap por que es un option con un result dentro
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
     if request_line == "GET / HTTP/1.1" {
         stream
             .write(parse_response("HTTP/1.1 200 OK", "./client/index.html").as_bytes())
             .unwrap();
-    } else {
+    }
+    else if request_line == "GET /sleep HTTP/1.1" {
+        thread::sleep(Duration::from_secs(5));
+        stream
+            .write(parse_response("HTTP/1.1 200 OK", "./client/index.html").as_bytes())
+            .unwrap();
+    } 
+    else {
         stream
             .write(parse_response("HTTP/1.1 404 NOT FOUND", "./client/404.html").as_bytes())
             .unwrap();
@@ -53,6 +54,5 @@ fn parse_response(status_line: &str, contents_path: &str) -> String {
             Content-Length: {length}\r\n\r\n\
             {contents}"
     );
-    // hay que pasarlo como bytes
     response
 }
--- src/threadpool.rs
+++ src/threadpool.rs
@@ -1,3 +1,4 @@
+
 use std::{
     sync::{
         mpsc::{self, Receiver},
@@ -6,51 +7,52 @@ use std::{
     thread,
 };
 
+/// Error types that may occur while creating or executing tasks in the thread pool.
 #[derive(Debug)]
 pub enum ThreadPoolError {
+    /// Error indicating a problem occurred during the creation of the thread pool.
     CreationError(String),
+    /// Error indicating a problem occurred during the execution of a task in the thread pool.
     ExecutionError(String),
 }
 
+/// A simple thread pool implementation.
 pub struct ThreadPool {
     workers: Vec<Worker>,
     sender: mpsc::Sender<Job>,
 }
 
+/// A worker thread in the thread pool.
 struct Worker {
     id: usize,
     thread: thread::JoinHandle<()>,
 }
 
+/// Type alias for a closure-based job that can be executed in the thread pool.
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
+    /// Constructs a new thread pool with the given number of worker threads and stack size in bytes.
+    ///
+    /// # Arguments
+    ///
+    /// * `size`: The number of worker threads in the thread pool.
+    /// * `stack_size_bytes`: The size of the stack for each worker thread in bytes.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing the created thread pool if successful, or an error of type `ThreadPoolError`.
+    ///
+    /// # Panics
+    ///
+    /// This function may panic if the `thread::Builder` fails to create a new thread.
     pub fn build(size: usize, stack_size_bytes: usize) -> Result<ThreadPool, ThreadPoolError> {
-        let spawn_workers = |size: usize, receiver: Arc<Mutex<Receiver<Job>>>| {
-            // usaremos el builder de thread ya que nos devuelve un error si falla al crear un thread
-            let mut workers: Vec<Worker> = Vec::with_capacity(size);
-            for id in 0..size {
-                let builder = thread::Builder::new();
-                let builder = builder.stack_size(stack_size_bytes);
-                let receiver_clone = Arc::clone(&receiver); // Clone the Arc to use inside the closure
-                let thread = builder
-                    .spawn(move || loop {
-                        let job = receiver_clone.lock().unwrap().recv().unwrap();
-                        job();
-                    })
-                    .unwrap();
-
-                workers.push(Worker { id, thread });
-            }
-            workers
-        };
-
         let (sender, receiver) = mpsc::channel();
         let receiver = Arc::new(Mutex::new(receiver));
 
         if size > 0 && size <= 10000 {
             Ok(ThreadPool {
-                workers: spawn_workers(size, Arc::clone(&receiver)),
+                workers: Self::spawn_workers(size, Arc::clone(&receiver), stack_size_bytes),
                 sender,
             })
         } else if size > 10000 {
@@ -64,6 +66,15 @@ impl ThreadPool {
         }
     }
 
+    /// Executes the given closure-based job in one of the worker threads of the thread pool.
+    ///
+    /// # Arguments
+    ///
+    /// * `f`: The closure-based job to be executed in the thread pool.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` indicating whether the job was successfully executed or an error of type `ThreadPoolError`.
     pub fn execute<F>(&self, f: F) -> Result<(), ThreadPoolError>
     where
         F: FnOnce() + Send + 'static,
@@ -78,4 +89,29 @@ impl ThreadPool {
             }
         }
     }
+
+    // Private method to spawn worker threads in the thread pool.
+    fn spawn_workers(
+        size: usize,
+        receiver: Arc<Mutex<Receiver<Job>>>,
+        stack_size_bytes: usize,
+    ) -> Vec<Worker> {
+        let mut workers: Vec<Worker> = Vec::with_capacity(size);
+        for id in 0..size {
+            let builder = thread::Builder::new();
+            let builder = builder.stack_size(stack_size_bytes);
+            let receiver_clone = Arc::clone(&receiver);
+            let thread = builder
+                .spawn(move || loop {
+                    let job = receiver_clone.lock().unwrap().recv().unwrap();       
+                    println!("Worker {id} got a job; executing.");
+                    job();
+                })
+                .unwrap();
+
+            workers.push(Worker { id, thread });
+        }
+        workers
+    }
 }
+
